4c9ad6def7dcaca7bd33758ea535990657a31698
Piotr Przybylak <piotr.przybylak@gmail.com>
2016-07-07 15:25:35 +0200

    Library singleton service class changed to use enum

--- src/main/java/it/morfoza/quickstart/library/Library.java
+++ src/main/java/it/morfoza/quickstart/library/Library.java
@@ -5,13 +5,13 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class Library {
+public enum Library {
 
-    public static final Library INSTANCE = new Library();
+    INSTANCE;
 
     private List<Book> books = new ArrayList<>();
 
-    private Library() {
+    Library() {
         books.add(new Book("War and Peace", "Leo Tolstoy"));
         books.add(new Book("Hollywood", "Charles Bukowski"));
     }
